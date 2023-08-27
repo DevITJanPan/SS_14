145c862cd86a689e34faf56b9298542f0f1d8911
DevITJaPan <leetuanjapan0201@gmail.com>
2023-08-27 12:44:04 +0900

    SS_14

---
+++ src/ra/IEntity.java
@@ -0,0 +1,11 @@
+package ra;
+
+import java.util.List;
+import java.util.Scanner;
+
+public interface IEntity<T> {
+    void inputData(Scanner scanner, List<T> list);
+    void displayData();
+    void calAge();
+    void calAvgMark_rank();
+}
